fe43839a99b548a54096c425d04703a93b8d5d39
rogue-shark <shantanuss789@gmail.com>
2022-09-24 11:31:43 +0530

    added clear function separately

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const type1Btn = document.querySelector('.type-1-btn');
 const container = document.querySelector('.container');
 const clearBtn = document.querySelector('.clear');
 
-type1Btn.addEventListener('click', gridGenerator)
 
-clearBtn.addEventListener('click', () => {
-    container.innerHTML = ''
-})
+
+type1Btn.addEventListener('click', () => {
+    gridGenerator()
+    console.log(type1Btn.value)
+} )
+
+clearBtn.addEventListener('click', clearFunc)
 
 function gridGenerator() {
     container.style.gridTemplateColumns = 'repeat(16, 1fr)'
     container.style.gridTemplateRows = 'repeat(16, 1fr)'
-
+    clearFunc()
     for(let i=0; i < 16 * 16; i++) {
         const gridEle = document.createElement('div')
         gridEle.classList.add('grid-element')
@@ -21,10 +24,11 @@ function gridGenerator() {
     }
 }
 
+//styling targeted event
 function changeColor(e) {
     if ( e.type === 'mouseover'){
         e.target.style.backgroundColor = 'black'
     }
 }
 
-
+function clearFunc() {container.innerHTML = ''}
